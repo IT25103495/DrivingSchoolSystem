5b5b9b4d0759f7fb6071a9d043f1b9fa90078713
Luqmaan M.I.A <it25103495@my.sliit.lk>
2026-05-05 23:52:41 +0530

    Added todo and fixme

--- src/main/java/com/wd44/drivingschoolsystem/Services/StudentService.java
+++ src/main/java/com/wd44/drivingschoolsystem/Services/StudentService.java
@@ -31,9 +31,11 @@ public class StudentService {
         return "Saved";
     }
 
+    //TODO: For the loveee of god make a mapper class pleaseeee (use @Mapper for fun shit)
+
     //Redundant code and wasted database lookup doing existsBy and then findBy, this was done to return a nice message besides 'internal server error'
     //See if there's a workaround
-    //Edit: Changed to return 404 not found, if that is hard to work with will change back or further modify
+    //FIXME: Changed to return 404 not found, if that is hard to work with will change back or further modify
     @Transactional
     public @ResponseBody String updateStudent(int ID, studentUpdateDTO _std) {
         Student std = studentRepo.findById(ID).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student Not Found!"));
