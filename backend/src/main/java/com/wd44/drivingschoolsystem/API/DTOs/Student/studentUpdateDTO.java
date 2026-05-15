7c5e7f93927fd4bbaf375b19c1cecb09b91b1006
Himath Sathmin <it25102292@my.sliit.lk>
2026-05-15 08:52:10 +0530

    added stuUpdateDTO

---
+++ backend/src/main/java/com/wd44/drivingschoolsystem/API/DTOs/Student/studentUpdateDTO.java
@@ -0,0 +1,82 @@
+package com.wd44.drivingschoolsystem.API.DTOs.Student;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+
+
+public class studentUpdateDTO {
+
+
+    @Id
+    protected int ID;
+
+    @Schema(defaultValue = "noChange")
+    protected String fullName = "noChange";
+
+    @Schema(defaultValue = "1920-01-01")
+    protected LocalDate dob = LocalDate.parse("1920-01-01");
+
+    @Schema(defaultValue = "noChange")
+    protected String email = "noChange";
+
+    @Schema(defaultValue = "noChange")
+    protected String phoneNum = "noChange";
+
+    @Schema(defaultValue = "noChange")
+    protected String username = "noChange";
+    @Schema(defaultValue = "noChange")
+    protected String password = "noChange";
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
