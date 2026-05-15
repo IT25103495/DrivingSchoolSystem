4874a5c1bbea4a0f5c97bdb6c5661c6bd7a89c41
Himath Sathmin <it25102292@my.sliit.lk>
2026-05-15 08:44:31 +0530

    added stuCreateDTO

---
+++ backend/src/main/java/com/wd44/drivingschoolsystem/API/DTOs/Student/studentCreateDTO.java
@@ -0,0 +1,22 @@
+package com.wd44.drivingschoolsystem.API.DTOs.Student;
+
+import java.time.LocalDate;
+
+public class studentCreateDTO {
+
+    public class studentCreateDTO {
+
+        protected String fullName;
+
+        protected LocalDate dob;
+
+        protected String email;
+
+        protected String phoneNum;
+
+        protected String username;
+
+        protected String password;
+
+
+    }
