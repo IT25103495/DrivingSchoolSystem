ae68360f74c8e256204bfba42a21d8b8313ab2e8
IT25103277 <IT25103277@my.sliit.lk>
2026-05-15 19:17:30 +0530

    Update

--- backend/src/main/java/com/wd44/drivingschoolsystem/API/Controllers/LessonController.java
+++ backend/src/main/java/com/wd44/drivingschoolsystem/API/Controllers/LessonController.java
@@ -51,4 +51,16 @@ public class LessonController {
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
+    
+   public class AuthEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+
 }
